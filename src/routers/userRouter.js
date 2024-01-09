4575ed4104f0b2ca26deba82be71399a9797592a
Renan <renan13.com@gmailcom>
2024-01-09 16:17:09 -0300

    corrigindo função para seguir pessoas 3

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,22 +1,23 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
-const friendsController = require('../controllers/friendsControllers')
+const friendsController = require("../controllers/friendsControllers");
 const checkToken = require("../middlewares/authMiddlewar");
 
-
 router
-    .post("/register", (req, res) => userController.Register(req, res));
-router
-    .post("/login", (req, res) => userController.Login(req, res));
-router
-    .get("/:name", checkToken, (req, res) => userController.FindName(req, res))
-    .get("/search/:name", checkToken, (req, res) => userController.FindSearch(req, res))
-router
-    .patch("/edit/user", checkToken, (req, res) => userController.Update(req, res));
-router
-    .post("/follow", checkToken, (req, res) => friendsController.addFriend(req, res))
-    .get("/friends/:name", checkToken, (req, res) => friendsController.FindFriends(req, res))
-
-    
+  .post("/register", (req, res) => userController.Register(req, res))
+  .post("/login", (req, res) => userController.Login(req, res))
+  .get("/search/:name", checkToken, (req, res) =>
+    userController.FindSearch(req, res)
+  )
+  .get("/friends/:name", checkToken, (req, res) =>
+    friendsController.FindFriends(req, res)
+  )
+  .get("/:name", checkToken, (req, res) => userController.FindName(req, res))
+  .patch("/edit/user", checkToken, (req, res) =>
+    userController.Update(req, res)
+  )
+  .post("/follow", checkToken, (req, res) =>
+    friendsController.addFriend(req, res)
+  );
 
 module.exports = router;
